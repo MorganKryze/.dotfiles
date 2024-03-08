8fe188e8aece2b398200360efdfff352441eb87c
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-03-08 21:20:29 +0000

    🌟 (symlinks) add file

---
+++ symlinks.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# Zsh
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/.zshenv ~/.zshenv
+ln -s ~/dotfiles/zsh/.zprofile ~/.zprofile
+
+ln -s ~/dotfiles/zsh/.exports ~/.exports
+ln -s ~/dotfiles/zsh/.aliases ~/.aliases
+ln -s ~/dotfiles/zsh/.functions ~/.functions
