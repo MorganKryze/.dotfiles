1bd562c7b7f7b2635c9f4d3d0cde1f7e0f5ee519
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-03-08 23:13:25 +0000

    🌟 add git and conda

--- symlinks.sh
+++ symlinks.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+# Git
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
+
 # Zsh
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/dotfiles/zsh/.zshenv ~/.zshenv
@@ -12,3 +15,6 @@ ln -s ~/dotfiles/zsh/.functions ~/.functions
 # Yabai
 ln -s ~/dotfiles/yabai/.yabairc ~/.yabairc
 ln -s ~/dotfiles/yabai/.skhdrc ~/.skhdrc
+
+# Conda
+ln -s ~/dotfiles/conda/.condarc ~/.condarc
