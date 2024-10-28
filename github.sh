0895e6f69cc4bac735ce02b0f7cfa3a0ed238cb9
MorganKryze <neoyega@icloud.com>
2024-10-28 16:15:52 +0100

    🚑(fix) update github script to avoid mismatch

--- github.sh
+++ github.sh
@@ -1,23 +1,21 @@
 source ~/.dotfiles/.env
 
 # Generate SSH key
-ssh-keygen -t ed25519 -C "$GITHUB_EMAIL"
+ssh-keygen -t ed25519 -C "$GITHUB_EMAIL" -f ~/.ssh/git -p ""
 
 # Start the ssh-agent in the background
 eval "$(ssh-agent -s)"
 
 # Add authorized host
 echo "Host github.com
-  IgnoreUnknown UseKeychain
-  AddKeysToAgent yes
-  UseKeychain yes
-  IdentityFile ~/.ssh/id_ed25519" > ~/.ssh/config
+  IPreferredAuthentications publickey
+  IdentityFile ~/.ssh/git" > ~/.ssh/config
 
 # Add your SSH private key to the ssh-agent and store your passphrase in the keychain
-ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+ssh-add --apple-use-keychain ~/.ssh/git
 
 # Copy the SSH key to your clipboard
-pbcopy < ~/.ssh/id_ed25519.pub
+pbcopy < ~/.ssh/git.pub
 
 # Open the GitHub settings page
 open "https://github.com/settings/keys"
