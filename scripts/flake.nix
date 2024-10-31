27aeb924d1e02f7472a3b2a8d24366bc50322a54
MorganKryze <neoyega@icloud.com>
2024-10-31 23:01:27 +0100

    🌟(nix) change username

--- scripts/flake.nix
+++ scripts/flake.nix
@@ -126,7 +126,7 @@
             LSQuarantine = false;
           };
           loginwindow = {
-            autoLoginUser = "Yann Vidamment";
+            autoLoginUser = "morgan";
             DisableConsoleAccess = true;
             GuestEnabled = false;
             LoginwindowText = "Praise the Greater Good!";
