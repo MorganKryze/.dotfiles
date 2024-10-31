2756fe3aa154b838716d2f3b1913117bb420bb8b
MorganKryze <neoyega@icloud.com>
2024-10-31 15:51:42 +0100

    🚑(nix) update repositories

--- scripts/flake.nix
+++ scripts/flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
