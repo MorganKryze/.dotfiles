6d1cb40da721c1ab9c3b66eb5185fcf5192f8fda
MorganKryze <neoyega@icloud.com>
2024-11-12 02:40:13 +0100

    🚑(nix) janky conf in good place

--- modules/home-manager/apps/jankyborders/default.nix
+++ modules/home-manager/apps/jankyborders/default.nix
@@ -4,5 +4,9 @@
   services.jankyborders = {
     enable = true;
     package = pkgs.jankyborders;
+    active_color = "0xffe1e3e4";
+    inactive_color = "0xff494d64";
+    hidpi = true;
+    width = 5.0;
   };
 }
