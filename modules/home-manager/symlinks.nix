1ac522184145c81561d94ab48c19104eabacfc3a
MorganKryze <neoyega@icloud.com>
2024-11-13 17:57:15 +0100

    🌟(nix) set symlinks to relative instead of absolute

--- modules/home-manager/symlinks.nix
+++ modules/home-manager/symlinks.nix
@@ -2,31 +2,31 @@
 
 {
   home.file = {
-    ".gitconfig".source = /Users/morgan/.dotfiles/modules/home-manager/apps/git/.gitconfig;
-    ".git_func".source = /Users/morgan/.dotfiles/modules/home-manager/apps/git/.git_func;
-    ".gitemojis/commit-msg".source = /Users/morgan/.dotfiles/modules/home-manager/apps/git/gitemojis/commit-msg;
-    ".gitemojis/prepare-commit-msg".source = /Users/morgan/.dotfiles/modules/home-manager/apps/git/gitemojis/prepare-commit-msg;
+    ".gitconfig".source = ./apps/git/.gitconfig;
+    ".git_func".source = ./apps/git/.git_func;
+    ".gitemojis/commit-msg".source = ./apps/git/gitemojis/commit-msg;
+    ".gitemojis/prepare-commit-msg".source = ./apps/git/gitemojis/prepare-commit-msg;
 
-    ".zshrc".source = /Users/morgan/.dotfiles/modules/home-manager/apps/zsh/.zshrc;
-    ".zshenv".source = /Users/morgan/.dotfiles/modules/home-manager/apps/zsh/.zshenv;
-    ".zprofile".source = /Users/morgan/.dotfiles/modules/home-manager/apps/zsh/.zprofile;
-    ".exports".source = /Users/morgan/.dotfiles/modules/home-manager/apps/zsh/.exports;
-    ".aliases".source = /Users/morgan/.dotfiles/modules/home-manager/apps/zsh/.aliases;
-    ".functions".source = /Users/morgan/.dotfiles/modules/home-manager/apps/zsh/.functions;
-    ".yabai_func".source = /Users/morgan/.dotfiles/modules/home-manager/apps/yabai/.yabai_func;
+    ".zshrc".source = ./apps/zsh/.zshrc;
+    ".zshenv".source = ./apps/zsh/.zshenv;
+    ".zprofile".source = ./apps/zsh/.zprofile;
+    ".exports".source = ./apps/zsh/.exports;
+    ".aliases".source = ./apps/zsh/.aliases;
+    ".functions".source = ./apps/zsh/.functions;
+    ".yabai_func".source = ./apps/yabai/.yabai_func;
 
-    ".condarc".source = /Users/morgan/.dotfiles/modules/home-manager/apps/conda/.condarc;
-    ".conda_func".source = /Users/morgan/.dotfiles/modules/home-manager/apps/conda/.conda_func;
+    ".condarc".source = ./apps/conda/.condarc;
+    ".conda_func".source = ./apps/conda/.conda_func;
 
-    "Library/Application Support/Code/User/settings.json".source = /Users/morgan/.dotfiles/modules/home-manager/apps/vscode/settings.json;
-    "Library/Application Support/Code/User/keybindings.json".source = /Users/morgan/.dotfiles/modules/home-manager/apps/vscode/keybindings.json;
+    "Library/Application Support/Code/User/settings.json".source = ./apps/vscode/settings.json;
+    "Library/Application Support/Code/User/keybindings.json".source = ./apps/vscode/keybindings.json;
 
     ".hushlogin".source = /Users/morgan/.dotfiles/docs/assets/.hushlogin;
-    ".curlrc".source = /Users/morgan/.dotfiles/modules/home-manager/apps/curl/.curlrc;
-    ".config/yazi/theme.toml".source = /Users/morgan/.dotfiles/modules/home-manager/apps/yazi/theme.toml;
-    ".config/yazi/yazi.toml".source = /Users/morgan/.dotfiles/modules/home-manager/apps/yazi/yazi.toml;
-    ".config/starship.toml".source = /Users/morgan/.dotfiles/modules/home-manager/apps/starship/starship.toml;
-    ".wezterm.lua".source = /Users/morgan/.dotfiles/modules/home-manager/apps/wezterm/.wezterm.lua;
-    ".config/fastfetch/config.jsonc".source = /Users/morgan/.dotfiles/modules/home-manager/apps/fastfetch/config.jsonc;
+    ".curlrc".source = ./apps/curl/.curlrc;
+    ".config/yazi/theme.toml".source = ./apps/yazi/theme.toml;
+    ".config/yazi/yazi.toml".source = ./apps/yazi/yazi.toml;
+    ".config/starship.toml".source = ./apps/starship/starship.toml;
+    ".wezterm.lua".source = ./apps/wezterm/.wezterm.lua;
+    ".config/fastfetch/config.jsonc".source = ./apps/fastfetch/config.jsonc;
   };
 }
